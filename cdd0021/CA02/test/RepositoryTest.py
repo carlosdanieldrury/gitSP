3fd7b1e2e72d10c76e754158d7d81c402a4cb075
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-10-08 18:08:47 -0500

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

--- cdd0021/CA02/test/RepositoryTest.py
+++ cdd0021/CA02/test/RepositoryTest.py
@@ -128,6 +128,14 @@ class RepositoryTest(unittest.TestCase):
         self.testRepository.addComponent(component1)
         self.testRepository.addComponent(component2)
         self.assertRaises(ValueError, self.testRepository.addComponent, component1)    
+        
+    def test_02_22_001_getRelativeSizeComponent(self):
+        self.testRepository = repository.Repository(5)
+        component1 = component.Component(name="Component01",methodCount=1,locCount=76)
+        component2 = component.Component("Component02",locCount=116,methodCount=4)
+        self.testRepository.addComponent(component1)
+        self.testRepository.addComponent(component2)
+        self.testRepository.getRelativeSize(component2)
     
 
 if __name__ == "__main__":

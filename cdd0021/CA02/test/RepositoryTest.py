b36f904d1cee68c47bc56f8a7d4db6efca14bf09
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-10-09 22:48:09 -0500

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

--- cdd0021/CA02/test/RepositoryTest.py
+++ cdd0021/CA02/test/RepositoryTest.py
@@ -202,6 +202,34 @@ class RepositoryTest(unittest.TestCase):
         self.testRepository.addComponent(component4)
         self.testRepository.addComponent(component5)
         self.assertRaises(ValueError, self.testRepository.estimateByRelativeSize, "X",0,"L")
+        
+    def test_02_23_004_estimateByRelativeSizeExc3(self):
+        self.testRepository = repository.Repository(5)
+        component1 = component.Component("Component01",1,1)
+        component2 = component.Component("Component02",2,4)
+        component3 = component.Component("Component03",3,6)
+        component4 = component.Component("Component04",4,8)
+        component5 = component.Component("Component05",5,10)
+        self.testRepository.addComponent(component1)
+        self.testRepository.addComponent(component2)
+        self.testRepository.addComponent(component3)
+        self.testRepository.addComponent(component4)
+        self.testRepository.addComponent(component5)
+        self.assertRaises(ValueError, self.testRepository.estimateByRelativeSize, "Y","X","L")
+        
+     def test_02_23_004_estimateByRelativeSizeExc3(self):
+        self.testRepository = repository.Repository(5)
+        component1 = component.Component("Component01",1,1)
+        component2 = component.Component("Component02",2,4)
+        component3 = component.Component("Component03",3,6)
+        component4 = component.Component("Component04",4,8)
+        component5 = component.Component("Component05",5,10)
+        self.testRepository.addComponent(component1)
+        self.testRepository.addComponent(component2)
+        self.testRepository.addComponent(component3)
+        self.testRepository.addComponent(component4)
+        self.testRepository.addComponent(component5)
+        self.assertRaises(ValueError, self.testRepository.estimateByRelativeSize, "Y",2 ,"X")   
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

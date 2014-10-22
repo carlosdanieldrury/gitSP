b3cbd8fd0db70d013a3a8149b3124d42073f6805
Carlos Daniel Drury <cdd0021@auburn.edu>
2014-10-21 22:54:30 -0500

    Signed-off-by: Carlos Daniel Drury <cdd0021@auburn.edu>

---
+++ cdd0021/CA03/prod/PythonScript.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 21, 2014
+
+@author: drury
+'''
+
+class PythonScript(object):
+    '''
+    PythonScript Class that read a python file and can count how many components there are inside the file and how many
+    functions as well.
+    '''
+
+
+    def __init__(self, fileName):
+        '''
+        Constructor
+        '''
+        self.fileName = fileName

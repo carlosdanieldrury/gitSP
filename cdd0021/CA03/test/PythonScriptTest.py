a15ac3b1406d29bff7154b195f703d8043344535
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-10-26 12:44:20 -0500

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

--- cdd0021/CA03/test/PythonScriptTest.py
+++ cdd0021/CA03/test/PythonScriptTest.py
@@ -68,6 +68,19 @@ class TestPythonScript(unittest.TestCase):
         except Exception as e:
             self.fail("incorrect exception was raised: " + str(e))      
         
+        
+    def test200_020_ShouldRaiseExceptionEmptyString(self):
+        expectedString = "PythonScript.__init__:  "
+        try:
+            testPythonScript = PythonScript.PythonScript("")                                              
+            self.fail("exception was not raised")                    
+        except ValueError as raisedException:                                           
+            diagnosticString = raisedException.args[0]                                   
+            self.assertEquals(expectedString, diagnosticString[0:len(expectedString)]) 
+        except AssertionError:
+            self.fail("exception was not raised") 
+        except Exception as e:
+            self.fail("incorrect exception was raised: " + str(e))
 
 
 if __name__ == "__main__":

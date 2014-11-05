ef6bb1ff1a6d9aaa9f04bb320d810adccb7c2fca
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-11-05 17:37:59 -0600

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

---
+++ cdd0021/CA04/prod/Project.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 5, 2014
+
+@author: drury
+'''
+
+class Project(object):
+    '''
+    Project Class
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        

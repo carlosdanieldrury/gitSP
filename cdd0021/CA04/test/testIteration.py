76cbc5c0315e425d7aeabeec082acd853e49f346
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-11-04 23:12:06 -0600

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

--- cdd0021/CA04/test/testIteration.py
+++ cdd0021/CA04/test/testIteration.py
@@ -23,6 +23,18 @@ class Test(unittest.TestCase):
     def test100_030_ShouldGetPV(self):
         myIteration = Iteration.Iteration(effort=120, plannedVelocity=3)
         self.assertEquals(myIteration.getPV(), 3)
+        
+#sad 
+    def test200_010_ShouldRaiseExceptionInvalidEffort(self):
+        expectedString = "Iteration.__init__:  "
+        try:
+            myIteration = Iteration(0,3)                                          
+            self.fail("exception was not raised")                    
+        except ValueError as raisedException:                                           
+            diagnosticString = raisedException.args[0]                                   
+            self.assertEquals(expectedString, diagnosticString[0:len(expectedString)]) 
+        except:
+            self.fail("incorrect exception was raised")     
          
 
 if __name__ == "__main__":

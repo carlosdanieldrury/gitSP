00d4134b4bf6ecc116bfb2b8e684966557c9e586
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-11-04 23:03:47 -0600

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

---
+++ cdd0021/CA04/prod/Iteration.py
@@ -0,0 +1,18 @@
+'''
+Created on Nov 4, 2014
+
+@author: drury
+'''
+
+class Iteration(object):
+    '''
+    Iteration Class - Parameters effort and plannedVelocity
+    '''
+
+
+    def __init__(self, effort, plannedVelocity):
+        '''
+        Constructor
+        '''
+        self.effort = effort
+        self.plannedVelocity = plannedVelocity

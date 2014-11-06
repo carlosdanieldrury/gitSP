4072e5e0b7b82f18df3faf123c5b588170b08fff
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-11-06 11:31:52 -0600

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

--- cdd0021/CA04/test/testSchedule.py
+++ cdd0021/CA04/test/testSchedule.py
@@ -47,6 +47,23 @@ class Test(unittest.TestCase):
         myCal.add(6, 90)
         mySched = Schedule.Schedule(myProject,myCal)
         self.assertEquals(mySched.getLastDay(), 5)
+        
+    def test100_040_ShouldReturnBurnDown(self):
+        iteration1 = Iteration.Iteration(30, 1)
+        iteration2 = Iteration.Iteration(60, 3)
+        myProject = Project.Project()
+        myProject.add(iteration1)
+        myProject.add(iteration2)
+        myCal = Calendar.Calendar()
+        myCal.add(1, 10)
+        myCal.add(2, 0)
+        myCal.add(3, 30)
+        myCal.add(4, 30)
+        myCal.add(5, 60)
+        myCal.add(6, 90)
+        mySched = Schedule.Schedule(myProject,myCal)
+        self.assertEquals(mySched.getBurnDown(day=3), 50)
+        
 
 
 

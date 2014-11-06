e80e5c73deb4d14ee2e62501ad80badd521de30b
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-11-06 10:07:21 -0600

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

---
+++ cdd0021/CA04/prod/Schedule.py
@@ -0,0 +1,18 @@
+'''
+Created on Nov 6, 2014
+
+@author: drury
+'''
+
+class Schedule(object):
+    '''
+    Schedule Class - Parameters: project and calendar
+    '''
+
+
+    def __init__(self, project, calendar):
+        '''
+        Constructor
+        '''
+        self.project = project
+        self.calendar = calendar

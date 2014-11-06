ee7f85ffebb09926045fe8b02c0e81449d7e0249
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2014-11-05 20:18:33 -0600

    Signed-off-by: Carlos Daniel Drury <carlosdanieldrury@hotmail.com>

--- cdd0021/CA04/test/testCalendar.py
+++ cdd0021/CA04/test/testCalendar.py
@@ -13,6 +13,11 @@ class Test(unittest.TestCase):
     def test100_010_ShouldConstructCalendar(self):
         self.assertIsInstance(Calendar.Calendar(), Calendar.Calendar)
 
+    def test100_020_ShouldAddDayEffort(self):
+        myCalendar = Calendar.Calendar()
+        day = 1
+        effort = 60
+        self.assertEquals(myCalendar.add(day,effort), 60)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
